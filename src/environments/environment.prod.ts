3efd412a8126e098756adebf1d370ed722d82f73
sakda homhuan <sakda.homhuan@gmail.com>
2019-10-08 16:07:01 +0700

    edit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAJkQNqYS4TAhd77J_ERTlPVJHkBI_T50k',
+    authDomain: 'map-insight.firebaseapp.com',
+    databaseURL: 'https://map-insight.firebaseio.com',
+    projectId: 'map-insight',
+    storageBucket: '',
+    messagingSenderId: '528513798407',
+    appId: '1:528513798407:web:497c1c20011ce65df235a2'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAJkQNqYS4TAhd77J_ERTlPVJHkBI_T50k',
+    authDomain: 'map-insight.firebaseapp.com',
+    databaseURL: 'https://map-insight.firebaseio.com',
+    projectId: 'map-insight',
+    storageBucket: '',
+    messagingSenderId: '528513798407',
+    appId: '1:528513798407:web:497c1c20011ce65df235a2'
+  }
 };
 
 /*
