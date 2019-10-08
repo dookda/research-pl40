6d5441e1ee83532f4e5a52b762e73c903c668c72
sakda homhuan <sakda.homhuan@gmail.com>
2019-10-08 21:19:10 +0700

    delete firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: 'AIzaSyAJkQNqYS4TAhd77J_ERTlPVJHkBI_T50k',
-    authDomain: 'map-insight.firebaseapp.com',
-    databaseURL: 'https://map-insight.firebaseio.com',
-    projectId: 'map-insight',
-    storageBucket: '',
-    messagingSenderId: '528513798407',
-    appId: '1:528513798407:web:497c1c20011ce65df235a2'
-  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyAJkQNqYS4TAhd77J_ERTlPVJHkBI_T50k',
-    authDomain: 'map-insight.firebaseapp.com',
-    databaseURL: 'https://map-insight.firebaseio.com',
-    projectId: 'map-insight',
-    storageBucket: '',
-    messagingSenderId: '528513798407',
-    appId: '1:528513798407:web:497c1c20011ce65df235a2'
-  }
 };
 
 /*
